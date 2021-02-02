4f6ebe31d8a2ff46c979d201efa8bb80efa8ab54
dotX12 <hash@shitposting.team>
2021-02-02 16:03:03 +0300

    📁 Logging the number of exchanges on accounts to json

--- tradeboost/utils/run.py
+++ tradeboost/utils/run.py
@@ -1,7 +1,10 @@
 from tradeboost.utils.send_and_accept import send_trade, accept_trade
+from tradeboost.utils.accounts_memory import MemoryAccount
 
 
 def run_program(account1, account2):
+
+    mem = MemoryAccount(account1.login, account2.login)
 
     while True:
         out = send_trade(account1)
@@ -9,3 +12,5 @@ def run_program(account1, account2):
 
         out2 = send_trade(account2)
         accept_trade(account1, out2)
+
+        mem.add_trade()
